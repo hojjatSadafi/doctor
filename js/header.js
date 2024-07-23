83d946446b1f6d71c656521f4e03ec70d805dc25
hojjat sadafi <161528589+hojjatSadafi@users.noreply.github.com>
2024-07-23 22:43:18 +0330

    refactor header and visit-page
    refactor the header: remove mobile menu and use one nav list
    refactor visit-page: change the colors and some responsive for two middle boxes

--- js/header.js
+++ js/header.js
@@ -10,13 +10,13 @@ $.addEventListener('scroll',function(){
     }
 })
 // mobile menu toggle
-const mobileMenu = $.querySelector('.header-mobile-nav-list')
+const mobileMenu = $.querySelector('.header-nav-wrapper')
 const hambergerMenu = $.querySelector('.header-hamberger')
-const headerXIcon = $.querySelector('.header-mobile-nav-list__x')
+const headerXIcon = $.querySelector('.header-nav-list__x')
 
 hambergerMenu.addEventListener('click',function(){
-    mobileMenu.classList.add('header-mobile-nav-list--open')
+    mobileMenu.classList.add('header-nav-wrapper--open')
 })
 headerXIcon.addEventListener('click',function(){
-    mobileMenu.classList.remove('header-mobile-nav-list--open')
+    mobileMenu.classList.remove('header-nav-wrapper--open')
 })
